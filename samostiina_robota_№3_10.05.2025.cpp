96b65e0438efefe32f84955f4b4e5a566b41961e
Євгеній Шевяков <evgenii.sheviakov@gmail.com>
2025-05-10 09:23:18 +0300

    Завдання №3

--- samostiina_robota_№3_10.05.2025.cpp
+++ samostiina_robota_№3_10.05.2025.cpp
@@ -1,63 +1,24 @@
 ﻿#include <iostream>
-#include <cmath>
+#include <string>
 
 using namespace std;
 
-class Triangle {
+class Book {
 private:
-    double a, b, c;
+    string title, author, genre;
+    int year;
 
-    bool isValid() const {
-        return (a + b > c) && (a + c > b) && (b + c > a);
-    }
-     
 public:
-    // Конструктор
-    Triangle(double side1, double side2, double side3) {
-        a = side1;
-        b = side2;
-        c = side3;
+    Book(string t, string a, string g, int y) : title(t), author(a), genre(g), year(y) {}
 
-        if (!isValid()) {
-            cerr << "Невірні сторони трикутника!" << endl;
-            a = b = c = 0;
-        }
+    void printInfo() const {
+        cout << "Title: " << title << "\nAuthor: " << author << "\nGenre: " << genre << "\nYear: " << year << endl;
     }
-
-    // Метод обчислення периметра
-    double getPerimeter() const {
-        return a + b + c;
-    }
-
-    // Метод обчислення площі за формулою Герона
-    double getArea() const {
-        double s = getPerimeter() / 2.0;
-        return sqrt(s * (s - a) * (s - b) * (s - c));
-    }
-
-    // Метод перевірки, чи є трикутник прямокутним
-    bool isRightAngled() const {
-        double aa = a * a, bb = b * b, cc = c * c;
-        return fabs(aa + bb - cc) < 1e-6 ||
-            fabs(aa + cc - bb) < 1e-6 ||
-            fabs(bb + cc - aa) < 1e-6;
-    }
-
-    // Геттери
-    double getA() const { return a; }
-    double getB() const { return b; }
-    double getC() const { return c; }
 };
 
 int main() {
-
-    system("chcp 1251 > nul"); //підключення української мови
-
-    Triangle t(3, 4, 5);
-
-    cout << "Периметр: " << t.getPerimeter() << endl;
-    cout << "Площа: " << t.getArea() << endl;
-    cout << "Прямокутний: " << (t.isRightAngled() ? "Так" : "Ні") << endl;
+    Book book("1984", "George Orwell", "Dystopian", 1949);
+    book.printInfo();
 
     return 0;
 }
